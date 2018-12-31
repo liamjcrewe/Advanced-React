bc4ae14083f3ad8c62cb4403c9fd5d09d38096f0
Liam Crewe <liam.crewe@mayden.co.uk>
2018-12-31 15:42:48 +0000

    Video #14 complete; item create/fetch with Prisma

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {}
+const Mutations = {
+  async createItem (parent, args, context, info) {
+    // TODO check if user logged in
+
+    const item = await context.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item
+  }
+}
 
 module.exports = Mutations
