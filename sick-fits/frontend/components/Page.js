594c428c6c7140e2bd42063211f91ec9bbaab06e
Liam Crewe <liam.crewe@mayden.co.uk>
2018-12-31 11:45:37 +0000

    Video #6 complete; styled components intro

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,38 @@
+import styled, { ThemeProvider } from 'styled-components'
+
 import Meta from './Meta'
 import Header from './Header'
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
+
 const Page = ({ children }) =>
-  <div>
-    <Meta />
-    <Header />
-    {children}
-  </div>
+  <ThemeProvider theme={theme}>
+    <StyledPage>
+      <Meta />
+      <Header />
+      <Inner>
+        {children}
+      </Inner>
+    </StyledPage>
+  </ThemeProvider>
 
 export default Page
